27501f02d2e61b27bbeba0a422643e066636a1bf
yeong2222 <tmddud000@naver.com>
2024-07-29 18:43:45 +0900

    yeong2

--- src/pages/mainservice/DetailedPage.js
+++ src/pages/mainservice/DetailedPage.js
@@ -13,12 +13,14 @@ import { grey } from "@mui/material/colors";
 
 export function DetailedPage() {
 
+
+
     return (
         <div>
             <Stack sx={{ alignItems: 'center', justifyItems: 'center' }}>
 
-                <Box sx={{ height: 130 }} />
-
+                <Box
+                    sx={{ height: 130 }} />
                 <Typography component="h1" variant="h4">
                     방제 신청서
                 </Typography>
@@ -27,41 +29,28 @@ export function DetailedPage() {
 
                 <Box sx={{ height: 70 }} />
 
-                <Box sx={{
-                    width: '40%',
-                    borderBottom: 1,
-                    borderColor: 'grey.500'
-                }}>
 
-                    <Grid
-                        container
-                        spacing={0}
-                        alignItems="center">
+
+
+                <Box sx={{ width: '40%', borderBottom: 1, borderColor: 'grey.500' }}>
+
+
+                    <Grid container spacing={0} alignItems="center">
 
                         <Grid item xs={2}>
                             <AccountCircleIcon sx={{ fontSize: 90, color: dark.contrastText }} />
                         </Grid>
 
                         <Grid item xs={2}>
-                            <Typography
-                                component="h1"
-                                variant="h6"
-                            >
-                                사용자 이름</Typography>
+                            <Typography component="h1" variant="h6" >사용자 이름</Typography>
                         </Grid>
 
                         <Grid item xs={4}>
                         </Grid>
-
+                        
                         <Grid item xs={4}>
-                            <Typography
-                                component="h1"
-                                variant="h6"
-                            >
-                                010 - xxxx - xxxx
-                            </Typography>
+                            <Typography component="h1" variant="h6" >010 - xxxx - xxxx</Typography>
                         </Grid>
-
                     </Grid>
                 </Box>
 
@@ -73,17 +62,14 @@ export function DetailedPage() {
 
 
                 <Grid container spacing={3} sx={{ width: '400px' }}>
-
                     <Grid item xs={5}>
-                        <Typography >수락 대기중</Typography>
+                        <Typography sx={{ border: 1, borderRadius: 2, color: 'lightblue', }}>수락 대기중</Typography>
                     </Grid>
-
                     <Grid item xs={3}>
-                        <Typography >진행중</Typography>
+                        <Typography sx={{ border: 1, borderRadius: 2, color: 'lightgreen' }}>진행중</Typography>
                     </Grid>
-
                     <Grid item xs={4}>
-                        <Typography >방제 완료</Typography>
+                        <Typography sx={{ border: 1, borderRadius: 2, color: 'grey.500' }}>방제 완료</Typography>
                     </Grid>
                 </Grid>
 
@@ -94,17 +80,20 @@ export function DetailedPage() {
 
 
                 <Box sx={{ width: '40%', borderBottom: 1, borderColor: 'grey.500' }}>
-                    <Stack spacing={5}>
-                        <Typography component="h1" variant="h6" >방제 주소</Typography>
-
-                        <Typography component="h1" variant="h6" >방제 규모</Typography>
-
-                        <Typography component="h1" variant="h6" >작물 종류</Typography>
-
-                        <Typography component="h1" variant="h6" >예약날짜</Typography>
-                    </Stack>
-
-
+                    <Grid container spacing={5} >
+                        <Grid item xs={12}>
+                            <Typography component="h1" variant="h6" >방제 주소</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography component="h1" variant="h6" >방제 규모</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography component="h1" variant="h6" >작물 종류</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography component="h1" variant="h6" >예약날짜</Typography>
+                        </Grid>
+                    </Grid>
                     <Box sx={{ height: 20 }} />
                 </Box>
 
@@ -112,43 +101,42 @@ export function DetailedPage() {
 
 
 
-                <Grid
-                    container spacing={5}
-                    sx={{ width: '350px' }}  >
-
+                <Grid container spacing={5} sx={{ width: '350px' }}  >
                     <Grid item xs={6} >
-                        <Typography  >방제 미완</Typography>
+                        <Typography sx={{ height: '80px', color: '#FFCC00', borderRadius: '10px', border: 1 }} >방제 미완</Typography>
                     </Grid>
-
                     <Grid item xs={6} >
-                        <Box>방제 완료</Box>
+                        <Box sx={{ height: '80px', backgroundColor: 'lightgreen', borderRadius: '10px', }} >방제 완료</Box>
                     </Grid>
                 </Grid>
 
                 <Box sx={{ height: '30px' }} />
 
 
-                <Button variant="contained">방제 완료로 변경하기</Button>
+                <Button variant="contained" sx={{ width: '400px', borderRadius: 2 }}>방제 완료로 변경하기</Button>
 
                 <Box sx={{ height: '30px' }} />
                 <Box sx={{ width: '350px' }}>
-                    <Card>
-                        <CardContent>
 
+                    <Card 
+                        sx={{ 
+                        height: 250,
+                        border: 1, 
+                        borderRadius: '25px', 
+                        borderColor: 'grey.500' }}>
+                            
+                        <CardContent>
                             <Typography gutterBottom variant="h5" component="h2">
                                 방제 업체
                             </Typography>
-
                             <Typography variant="body2" color="text.secondary">
                                 로고
                             </Typography>
-
-                            <Stack spacing={3}>
-                                <Typography>방제 업체 정보</Typography>
-                                <Typography>방제 업체 정보</Typography>
-                                <Typography>방제 업체 정보</Typography>
-                            </Stack>
-
+                            <Grid container spacing={5}>
+                                <Grid item xs={12}>방제 업체 정보</Grid>
+                                <Grid item xs={12}>방제 업체 정보</Grid>
+                                <Grid item xs={12}>방제 업체 정보</Grid>
+                            </Grid>
                         </CardContent>
 
                     </Card>
