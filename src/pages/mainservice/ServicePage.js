c9b90cc2789be9d9f11e57758f19207d25401d45
yeong2222 <tmddud000@naver.com>
2024-07-29 18:40:52 +0900

    yeong

--- src/pages/mainservice/ServicePage.js
+++ src/pages/mainservice/ServicePage.js
@@ -53,93 +53,123 @@ export function ServicePage() {
 
     // useState(getCustomerRequests(userInfo));
     //console.log(customerRequests)
-   // useEffect(() => {
-        // if ((!userInfo.success)) {
-          //   navigate("/login");
+    // useEffect(() => {
+    // if ((!userInfo.success)) {
+    //   navigate("/login");
     //    }
-     //    getCustomerRequests(userInfo, getRequests);
-  //  }, []) // 무한로딩 방지용 [] 추가 - 여기안에 들어가는 값을 이전값과 비교후 변화유무에 따라 새로고침한다
+    //    getCustomerRequests(userInfo, getRequests);
+    //  }, []) // 무한로딩 방지용 [] 추가 - 여기안에 들어가는 값을 이전값과 비교후 변화유무에 따라 새로고침한다
 
     //console.log(customerRequests)
     //console.log(customerRequests.length)
 
     return (
-    
+
         <div>
 
-            <Box sx={{height:130}}></Box>
+            <Box sx={{ height: 130 }}></Box>
 
-           <Grid container spacing={2}>
+            <Grid container spacing={2}>
 
-            <Grid item xs={3} > 
-            <Stack sx={{alignItems:'center',borderRight:1}}>
-            <Box height={40}></Box>
-            <AccountCircleIcon sx={{ fontSize: 100, color: dark.contrastText }} />
-            {/* <text>{userInfo.authenticatedUser.email}</text>  */}
-            <Button
-               // type="submit" // submit 버튼은 페이지를 새로고침해버린다
-                type="button" // 페이지를 새로고침하지 않는다 - 개발할때 console확인은 이걸로 하기
+                <Grid item xs={3} >
+                    <Stack
+                        spacing={3}
+                        sx={{ alignItems: 'center', borderRight: 1 }}>
 
-                variant="contained"
-                sx={{ mt: 3, mb: 2 ,height: 30}}
-                href='/requestform'
+                        <Box height={40}></Box>
 
-            >
-                서비스 신청 작성
-            </Button>
-        
-            <Box height={40}>신청서 현황</Box>
-            <Box height={40}>확인 대기목록</Box>
-            <Box height={40}>완료 내역</Box>
-            <Box height={40}></Box>
+                        <Typography>사용자 닉네임</Typography>
+                        {/* <text>{userInfo.authenticatedUser.email}</text>  */}
+                        <Button
+                            // type="submit" // submit 버튼은 페이지를 새로고침해버린다
+                            type="button" // 페이지를 새로고침하지 않는다 - 개발할때 console확인은 이걸로 하기
+                            variant="contained"
+                            href='/requestform'
 
-            </Stack>
-            </Grid>
-            
+                        >
+                            서비스 신청 작성
+                        </Button>
 
-            <Grid item xs={9}>
-            <Grid sx={{ flexGrow: 1, }} container spacing={2}>
-                <Grid item xs={12}>
-                    <Grid container justifyContent="center" spacing={10}>
-                        {customerRequests.map((value) => (
-                            <Grid key={value.id} item>
-                                <Card sx={{ minWidth: 275,height:300, border:1, borderRadius: '25px',}}>
-                                <Box sx={{alignContent: 'center', justifyContent: 'center',  }}>
-                                    <CardContent>
-                                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                                            {value.ownerName}
-                                        </Typography>
-                                        <Typography variant="h5" component="div">
-                                            {value.requestContent}
-                                        </Typography>
-                                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                            {value.address}
-                                        </Typography>
+                        <Typography>신청서 현황</Typography>
+                        <Typography>확인 대기목록</Typography>
+                        <Typography>완료 내역</Typography>
 
-                                        <Typography variant="body2">
-                                            {value.ownerEmail}
-                                            <br />
-                                            {value.reservationDate}
-                                        </Typography>
 
-                                    </CardContent>
-                                    <Box sx={{height:'75px'}}></Box>
-                                    <CardActions sx={{ justifyContent: "center", }}>
-                                        <Button
-                                        href='/detail' 
-                                        variant="contained"
-                                        >자세히 보기</Button>
-                                    </CardActions>
-                                   
-                                </Box>
-                                </Card>
+                    </Stack>
+                </Grid>
+
+
+                <Grid item xs={9}>
+                    <Grid
+                        sx={{ flexGrow: 1, }}
+                        container
+                        spacing={2}>
+
+                        <Grid item xs={12}>
+
+                            <Grid
+                                container
+                                justifyContent="center"
+                                spacing={10}>
+                                {customerRequests.map((value) => (
+                                    <Grid
+                                        key={value.id}
+                                        item>
+
+                                        <Card
+                                            sx={{
+                                                minWidth: 275,
+                                                height: 300,
+                                                border: 1,
+                                                borderRadius: '25px',
+                                            }}>
+
+                                            <Box
+                                                sx={{
+                                                    alignContent: 'center',
+                                                    justifyContent: 'center',
+                                                }}>
+                                                <CardContent>
+
+                                                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                                        {value.ownerName}
+                                                    </Typography>
+
+                                                    <Typography variant="h5" component="div">
+                                                        {value.requestContent}
+                                                    </Typography>
+
+                                                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                                        {value.address}
+                                                    </Typography>
+
+                                                    <Typography variant="body2">
+                                                        {value.ownerEmail}
+                                                        <br />
+                                                        {value.reservationDate}
+                                                    </Typography>
+
+                                                </CardContent>
+
+                                                <Box sx={{ height: '75px' }}></Box>
+
+                                                <CardActions sx={{ justifyContent: "center", }}>
+                                                    
+                                                    <Button
+                                                        href='/detail'
+                                                        variant="contained"
+                                                    >자세히 보기</Button>
+                                                </CardActions>
+
+                                            </Box>
+                                        </Card>
+                                    </Grid>
+                                ))}
                             </Grid>
-                        ))}
+                        </Grid>
                     </Grid>
                 </Grid>
             </Grid>
-            </Grid> 
-        </Grid>
         </div>
     );
 }
