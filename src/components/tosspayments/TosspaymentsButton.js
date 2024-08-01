f39c6126152f07aaa8542a0153481fd2a7483a2d
Chae Chulyoung <ccy09671324@gmail.com>
2024-08-01 16:12:50 +0900

    toss

--- src/components/tosspayments/TosspaymentsButton.js
+++ src/components/tosspayments/TosspaymentsButton.js
@@ -1,20 +1,22 @@
-import { Button } from "@mui/material";
 import { loadTossPayments, ANONYMOUS } from "@tosspayments/tosspayments-sdk";
 import { useEffect, useState } from "react";
+import { requestPayment } from "./tosspayments_func";
 
 // ------  SDK 초기화 ------
+// TODO: clientKey는 개발자센터의 API 개별 연동 키 > 결제창 연동에 사용하려할 MID > 클라이언트 키로 바꾸세요.
+// TODO: server.js 의 secretKey 또한 결제위젯 연동 키가 아닌 API 개별 연동 키의 시크릿 키로 변경해야 합니다.
+// TODO: 구매자의 고유 아이디를 불러와서 customerKey로 설정하세요. 이메일・전화번호와 같이 유추가 가능한 값은 안전하지 않습니다.
 // @docs https://docs.tosspayments.com/sdk/v2/js#토스페이먼츠-초기화
 const clientKey = "test_ck_LlDJaYngro2ZZaqGR00xVezGdRpX";
-const customerKey = "dnL2Z76H8RnFG-egzazSC"; // 로그인 정보의 uuid넣기
+const customerKey = generateRandomString(); // 유저 uuid 넣기
 
-export function TosspaymentsButton() {
+const amount = {
+    currency: "KRW",
+    value: 1000,
+};
 
+export function PaymentCheckoutPage() {
     const [payment, setPayment] = useState(null);
-
-    const [amount] = useState({
-        currency: "KRW",
-        value: 1,
-    });
 
     const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(null);
 
@@ -44,35 +46,29 @@ export function TosspaymentsButton() {
         fetchPayment();
     }, [clientKey, customerKey]);
 
-    // ------ '결제하기' 버튼 누르면 결제창 띄우기 ------
-    // @docs https://docs.tosspayments.com/sdk/v2/js#paymentrequestpayment
-    async function requestPayment() {
-        // 결제를 요청하기 전에 orderId, amount를 서버에 저장하세요.
-        // 결제 과정에서 악의적으로 결제 금액이 바뀌는 것을 확인하는 용도입니다.
-        await payment.requestPayment({
-            method: "CARD", // 카드 및 간편결제
-            amount: amount,
-            orderId: "BEsvs3eDmnU8DAHKB1Kc3", // 고유 주분번호
-            orderName: "토스 티셔츠 외 2건",
-            successUrl: window.location.origin + "/success", // 결제 요청이 성공하면 리다이렉트되는 URL
-            failUrl: window.location.origin + "/fail", // 결제 요청이 실패하면 리다이렉트되는 URL
-            customerEmail: "customer123@gmail.com",
-            customerName: "김토스",
-            customerMobilePhone: "01012341234",
-            // 카드 결제에 필요한 정보
-            card: {
-                useEscrow: false,
-                flowMode: "DEFAULT", // 통합결제창 여는 옵션
-                useCardPoint: false,
-                useAppCardOnly: false,
-            },
-        });
-    }
-
     return (
-        // 결제하기 버튼
-        <Button className="tosspaymentsbutton" onClick={() => requestPayment()}>
-            결제하기
-        </Button>
+        <div className="wrapper">
+            <div className="box_section">
+                <h1>일반 결제</h1>
+                <div id="payment-method" style={{ display: "flex" }}>
+                    <button id="CARD" className={`button2 ${selectedPaymentMethod === "CARD" ? "active" : ""}`} onClick={() => selectPaymentMethod("CARD")}>
+                        카드
+                    </button>
+                    <button id="TRANSFER" className={`button2 ${selectedPaymentMethod === "TRANSFER" ? "active" : ""}`} onClick={() => selectPaymentMethod("TRANSFER")}>
+                        계좌이체
+                    </button>
+                    <button id="VIRTUAL_ACCOUNT" className={`button2 ${selectedPaymentMethod === "VIRTUAL_ACCOUNT" ? "active" : ""}`} onClick={() => selectPaymentMethod("VIRTUAL_ACCOUNT")}>
+                        가상계좌
+                    </button>
+                </div>
+                <button className="button" onClick={() => requestPayment(payment)}>
+                    결제하기
+                </button>
+            </div>
+        </div>
     );
+}
+
+function generateRandomString() {
+    return window.btoa(Math.random().toString()).slice(0, 20);
 }
