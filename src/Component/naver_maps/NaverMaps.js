c0cfd7e6e475a077aef4f23bd7d75a96e7184121
ADCAT-01 <limlike3238@chosun.kr>
2024-09-21 05:44:31 +0900

    navermaps re

--- src/Component/naver_maps/NaverMaps.js
+++ src/Component/naver_maps/NaverMaps.js
@@ -1,124 +1,47 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
-export function NaverMap({ center = { lat: 37.3595316, lng: 127.1052133 }, zoom = 15, address }) {
-    const mapRef = useRef(null);
+const NaverMap = ({ center = { lat: 37.3595316, lng: 127.1052133 }, zoom = 15 }) => {
+  const mapRef = useRef(null);
 
-    useEffect(() => {
-        const { naver } = window;
+  useEffect(() => {
+    const { naver } = window;
 
-        if (!naver) return;
+    if (!naver || !mapRef.current) return;
 
-        const map = new naver.maps.Map(mapRef.current, {
-            center: new naver.maps.LatLng(center.lat, center.lng),
-            zoom,
-            mapTypeControl: true,
+    const map = new naver.maps.Map(mapRef.current, {
+      center: new naver.maps.LatLng(center.lat, center.lng),
+      zoom,
+      mapTypeControl: true,
+    });
+
+    // 지도 클릭 이벤트 추가
+    naver.maps.Event.addListener(map, 'click', function(e) {
+      const latlng = e.coord;
+
+      if (naver.maps.Service) {
+        // reverseGeocode 호출
+        naver.maps.Service.reverseGeocode({
+          coords: latlng,
+          orders: [
+            naver.maps.Service.OrderType.ADDR,
+            naver.maps.Service.OrderType.ROAD_ADDR
+          ].join(',')
+        }, function(status, response) {
+          if (status === naver.maps.Service.Status.ERROR) {
+            return alert('Something went wrong!');
+          }
+
+          const items = response.v2.results;
+          const address = items.length > 0 ? items[0].name : 'No address found';
+          alert(`Address: ${address}`);
         });
+      } else {
+        alert('Geocoding service is not available.');
+      }
+    });
+  }, [center, zoom]);
 
-        const infoWindow = new naver.maps.InfoWindow({
-            anchorSkew: true,
-        });
-
-        // 클릭 이벤트 등록
-        naver.maps.Event.addListener(map, 'click', function (e) {
-            searchCoordinateToAddress(e.coord, map, infoWindow);
-        });
-
-        // 주소로 좌표 검색 및 지도 업데이트
-        if (address) {
-            searchAddressToCoordinate(address, map, infoWindow);
-        }
-    }, [center, zoom, address]);
-
-    // 좌표 -> 주소 검색 함수
-    const searchCoordinateToAddress = (latlng, map, infoWindow) => {
-        infoWindow.close();
-
-        window.naver.maps.Service.reverseGeocode({
-            coords: latlng,
-            orders: [
-                window.naver.maps.Service.OrderType.ADDR,
-                window.naver.maps.Service.OrderType.ROAD_ADDR
-            ].join(','),
-        }, function (status, response) {
-            if (status === window.naver.maps.Service.Status.ERROR) {
-                return alert('Something went wrong!');
-            }
-
-            const items = response.v2.results;
-            let address = '';
-            let htmlAddresses = [];
-
-            for (let i = 0, ii = items.length; i < ii; i++) {
-                const item = items[i];
-                address = makeAddress(item) || '';
-                const addrType = item.name === 'roadaddr' ? '[도로명 주소]' : '[지번 주소]';
-                htmlAddresses.push((i + 1) + '. ' + addrType + ' ' + address);
-            }
-
-            infoWindow.setContent([
-                '<div style="padding:10px;min-width:200px;line-height:150%;">',
-                '<h4 style="margin-top:5px;">검색 좌표</h4><br />',
-                htmlAddresses.join('<br />'),
-                '</div>'
-            ].join('\n'));
-
-            infoWindow.open(map, latlng);
-        });
-    };
-
-    // 주소 -> 좌표 검색 함수
-    const searchAddressToCoordinate = (address, map, infoWindow) => {
-        window.naver.maps.Service.geocode({
-            query: address
-        }, function (status, response) {
-            if (status === window.naver.maps.Service.Status.ERROR) {
-                return alert('Something went wrong!');
-            }
-
-            if (response.v2.meta.totalCount === 0) {
-                return alert('No results found');
-            }
-
-            const item = response.v2.addresses[0];
-            const point = new window.naver.maps.Point(item.x, item.y);
-
-            const htmlAddresses = [];
-            if (item.roadAddress) htmlAddresses.push('[도로명 주소] ' + item.roadAddress);
-            if (item.jibunAddress) htmlAddresses.push('[지번 주소] ' + item.jibunAddress);
-            if (item.englishAddress) htmlAddresses.push('[영문 주소] ' + item.englishAddress);
-
-            infoWindow.setContent([
-                '<div style="padding:10px;min-width:200px;line-height:150%;">',
-                '<h4 style="margin-top:5px;">검색 주소 : ' + address + '</h4><br />',
-                htmlAddresses.join('<br />'),
-                '</div>'
-            ].join('\n'));
-
-            map.setCenter(point);
-            infoWindow.open(map, point);
-        });
-    };
-
-    // 주소 만들기 함수
-    const makeAddress = (item) => {
-        if (!item) return '';
-
-        const { region, land } = item;
-        let address = '';
-        if (region.area1.name) address += region.area1.name;
-        if (region.area2.name) address += ' ' + region.area2.name;
-        if (region.area3.name) address += ' ' + region.area3.name;
-        if (land && land.number1) {
-            address += ' ' + land.number1;
-            if (land.number2) address += '-' + land.number2;
-        }
-
-        return address;
-    };
-
-    return (
-        <div ref={mapRef} style={{ width: '100%', height: '400px' }} />
-    );
-}
+  return <div ref={mapRef} style={{ width: "100%", height: "100%" }} />;
+};
 
 export default NaverMap;
