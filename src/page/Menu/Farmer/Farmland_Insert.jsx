0f3cec2af2287dac9d8667e654431d4d24008ee4
ADCAT-01 <limlike3238@chosun.kr>
2024-09-26 22:25:48 +0900

    farminsert search func

--- src/page/Menu/Farmer/Farmland_Insert.jsx
+++ src/page/Menu/Farmer/Farmland_Insert.jsx
@@ -93,6 +93,10 @@ const Farmland_Insert = () => {
   const [farmlandm2, setFarmlandm2] = useState("");
   const [plant, setPlant] = useState("");
   const [check, setCheck] = useState(false);
+  const [cdtoken, setcdtoken] = useState("");
+  const [jibunCd, setJibunCd] = useState("");
+  const [adpnu, setPnu] = useState("");
+  const [adlndpclAr, setlndpclAr] = useState("");
 
   const setting_name = (e) => setFarmlandname(e.target.value);
   const setting_area = (e) => setFarmlandArea(e.target.value);
@@ -110,11 +114,11 @@ const Farmland_Insert = () => {
       navermapsy: window.addressInfo.y,
       detailAddress: "더미 상세 주소",
     },
-    pnu: "더미 Pnu",
+    pnu: adpnu,
     idCodeNm: "더미",
     mnnmSlno: "더미",
     regstrSeCodeNm: "더미",
-    lndpclAr: farmlandArea,
+    lndpclAr: adlndpclAr,
     posesnSeCodeNm: "더미",
     cnrsPsnCo: "더미",
     lastUpdtDt: "더미",
@@ -127,55 +131,86 @@ const Farmland_Insert = () => {
 
   //농지 주소 -> PNU 정보 변환
   const get_pnu_api = async () => {
-
     const digitalTwin = "https://api.vworld.kr/req/search?key=6C934ED4-5978-324D-B7DE-AC3A0DDC3B38"
-
     $.ajax(
       {
         type: "GET",
-        url: digitalTwin + "&request=search" + "&query=전라남도 나주시 토계동 25-5" + "&type=address" + "&category=parcel" + "&format=json",
+        url: digitalTwin + "&request=search" + `&query=${window.addressInfo.jibunAddress}` + "&type=address" + "&category=parcel" + "&format=json",
         dataType: "jsonp",
         success: function (res) {
           //const data = res.json()
           //console.log(data)
           //console.log(res)
           console.log(res.response.result.items)
-          console.log(res.response.result.items[0].id)
+          setPnu(res.response.result.items[0].id)
         },
         error: function (e) {
           alert(e.responseText);
         }
       });
-    // const res = await fetch(digitalTwin + "&request=search" + "&query=전라남도 나주시 토계동 25-5" + "&type=address" + "&category=parcel" + "&format=json", {
-    //   method: 'GET',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   //dataType: 'jsonp',
-    // })
-    // // .then((res) => res.json())
-    // // .then((data) => { res = data })
-    //console.log(res)
-
   };
+  // 주소검색으로 농지 제곱미터 받는 api
+  const search_area_api = async () => {
+    const area_search = "https://api.vworld.kr/ned/data/ladfrlList?key=6C934ED4-5978-324D-B7DE-AC3A0DDC3B38"
+    $.ajax(
+      {
+        type: "POST",
+        url: area_search + `&pnu=${adpnu}` +"&format=json",
+        dataType: "jsonp",
+        success: function (respnu) {
+          //const data = res.json()
+          //console.log(data)
+          //console.log(res)
+          setlndpclAr(respnu.ladfrlVOList.ladfrlVOList[0].lndpclAr)
+        },
+        // 왜인지 모르겠으나 alert로 undefined가 뜨고 있음
+        /*error: function (e) {
+          alert(e.responseText);
+        }*/
+      });
+  };
+  // cd값을 받기 위한 엑세스토큰 발급 API
+  const cd_for_accessToken = async () => {
+    const acc = await fetch("https://sgisapi.kostat.go.kr/OpenAPI3/auth/authentication.json?consumer_key=fb0450ed86ba405ba3ec&consumer_secret=a7ec04e5c1f8401594d5", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    const accres = await acc.json();
+    const accessToken = accres.result.accessToken;
+    setcdtoken(accessToken);
+    return accessToken;
+  }
+  // 발급받은 토큰으로 주소검색하여 cd값 받기
+  const get_cd_api = async (cdAccesstoken) => {
+    const cdval = await fetch(`https://sgisapi.kostat.go.kr/OpenAPI3/addr/geocode.json?address=${farmlandAddr}&accessToken=${cdAccesstoken}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    const cdres = await cdval.json();
+    const addcd = cdres.result.resultdata[0].adm_cd;
+    setJibunCd(addcd);
+  };
+  
 
   // 주소 찾기
-  const search_addr_API = () => {
+  const search_addr_API = async() => {
     if (!farmlandAddr) {
       return alert("농지 주소를 입력하세요.");
     }
 
     if (globalSearchAddressToCoordinate) {
       globalSearchAddressToCoordinate(farmlandAddr); // Naver Map API를 통해 주소 검색
-    } else {
-      alert("지도가 아직 로드되지 않았습니다.");
     }
   };
 
   // 농지 등록
   const insert_API = async () => {
-    const userInfo = JSON.parse(localStorage.getItem('User_info'));
-    const accessToken = userInfo?.access;
+    const userInfo = JSON.parse(localStorage.getItem('User_Credential'));
+    const accessToken = userInfo.access_token;
 
     const res = await fetch("https://192.168.0.28/customer/lands/", {
       method: "POST",
@@ -190,6 +225,17 @@ const Farmland_Insert = () => {
     const result = await res.json();
     console.log("Success:", result);
 
+  };
+  //주소찾기를 클릭하면 순차적으로 실행되도록 하는 함수
+  const handleSearch = async () => {
+    if (!farmlandAddr) {
+      return alert("농지 주소를 입력하세요.");
+    }
+      await search_addr_API();
+      const token = await cd_for_accessToken();
+      await get_cd_api(token);
+      await get_pnu_api();
+      await search_area_api();
   };
 
   // 면적 div태그 css
@@ -206,8 +252,17 @@ const Farmland_Insert = () => {
         <InsertBox>
           <div className="title">농지등록</div>
 
+          <Btn className="small" onClick={cd_for_accessToken}>
+            cd for accessToken
+          </Btn>
+          <Btn className="small" onClick={get_cd_api}>
+            cd값 가져오기
+          </Btn>
           <Btn className="small" onClick={get_pnu_api}>
-            주소 찾기
+            vworld PNU로 토지임야 주소, 평수 찾기
+          </Btn>
+          <Btn className="small" onClick={search_area_api}>
+           네이버 주소 찾기
           </Btn>
 
           <div className="subtitle">농지 별명</div>
@@ -225,11 +280,11 @@ const Farmland_Insert = () => {
               onChange={(e) => setFarmlandAddr(e.target.value)}
               onKeyPress={(e) => {
                 if (e.key === 'Enter') {
-                  search_addr_API();
+                  handleSearch();
                 }
               }}
             />
-            <Btn className="small" onClick={search_addr_API}>
+            <Btn className="small" onClick={handleSearch}>
               주소 찾기
             </Btn>
           </RowView2>
